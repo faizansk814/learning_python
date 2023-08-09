3f074a53949864677c071838ea2bca8524b3d4be
faizansk <faizansk814@gmail.com>
2023-08-09 20:19:02 +0530

    ok

--- crudapp/home/urls.py
+++ crudapp/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create',views.Post,name='post'),
+    path('',views.Post,name='post'),
     path('get',views.Get,name='get'),
     path('update',views.Update,name='update'),
     path('delete',views.Delete,name='delete')
